4650408ac1a84689a9f7af55d52d2d5fc4b97dd9
Alan <alaansannchezz@gmail.com>
2022-09-10 16:39:36 -0300

    Finalizacion de pagina principal

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,4 +1,11 @@
 <div class="contenedor-main">
+    <div class="contenedor">
+        <button class="portfolio-return">
+            <span class="arrow-btn"><i class='bx bx-left-arrow-alt'></i></span>
+            <span class="texto-btn">Ir a portafolio</span>
+        </button>
+    </div>
+
 
     <div class="contenedor-credenciales">
 
@@ -10,16 +17,29 @@
         </div>
 
     </div>
+    <?php
+    if (!$usuario) { ?>
+        <div class="container">
+            <div class="cards">
+                <div class="card card-one" id="cardLog">
+                    <h2 class="card-title">Log In</h2>
+                </div>
 
-    <div class="container">
-        <div class="cards">
-            <div class="card card-one">
-                <h2 class="card-title">Log In</h2>
-            </div>
-
-            <div class="card card-two">
-                <h2 class="card-title">Register</h2>
+                <div class="card card-two" id="cardRegister">
+                    <h2 class="card-title">Register</h2>
+                </div>
             </div>
         </div>
-    </div>
+
+    <?php } else { ?>
+        <div class="container">
+
+            <div class="cards">
+                <div class="card card-one" id="cardDashboard">
+                    <h2 class="card-title">Ir a dashboard</h2>
+                </div>
+            </div>
+        </div>
+    <?php }; ?>
 </div>
+<script src="/build/js/main.js"></script>
